25865e6fea712765d1ca840eda74cfc4ea43f3a3
logicrius <113976617+logicrius@users.noreply.github.com>
2023-04-16 21:41:39 +0700

    menambahkan API mahasiswa
    menambahkan API mahasiswa

---
+++ tpmodul9_1302210070/Controllers/MahasiswaCont.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tpmodul9_1302210070.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class mahasiswaController : ControllerBase
+    {
+        public static List<Mahasiswa> ListMahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa ("Muhammad Risjad Shidqi Febian","1302213067"),
+            new Mahasiswa ("Zhafran Hafizh Izdihar Riyadi","1302210125"),
+            new Mahasiswa ("Stevent Rangga Ramaditya","1302210097"),
+            new Mahasiswa ("Ashar Syahbudi Saleh","1302213016"),
+            new Mahasiswa ("Gifari Juliandri","1302213074"),
+            new Mahasiswa ("Surya Aulia","1302213116" )
+        };
+
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return ListMahasiswa;
+        }
+
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return ListMahasiswa[id];
+        }
+
+
+        [HttpPost]
+        public void Post(Mahasiswa m)
+        {
+            ListMahasiswa.Add(m);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            ListMahasiswa.RemoveAt(id);
+        }
+
+
+    }
+}
---
+++ tpmodul9_1302210070/Mahasiswa.cs
@@ -0,0 +1,14 @@
+﻿namespace tpmodul9_1302210070
+{
+    public class Mahasiswa
+    {
+        public String Nama { get; private set; }
+        public String Nim { get; private set; }
+
+        public Mahasiswa(String nama, String nim)
+        {
+            this.Nama = nama;
+            this.Nim = nim;
+        }
+    }
+}
